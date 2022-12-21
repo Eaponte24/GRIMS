e01ae6bdf8b25fce2a67b724147e54f28b46954e
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-12-21 10:32:44 -0500

    added functionality to the home routes and tested in insomnia

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+
+router.use("/", homeRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
