64dd3c486656ec14cffe8fcd87a8d51b27dbd0f7
Jakub Cichon <jakub@uconn.edu>
2022-12-20 21:20:04 -0500

    database set up and server boilerplate

---
+++ seeds/userSeeds.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+
+const userData = [
+  {
+    username: 'ricky',
+    password: 'password123',
+  },
+  {
+    username: 'ama',
+    password: 'password123',
+  },
+  {
+    username: 'robert',
+    password: 'password123'
+  },
+  {
+    username: 'jakub',
+    password: 'password123',
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
