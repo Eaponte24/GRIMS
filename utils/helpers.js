05a737c0c3342cad5317599ed2035f742de741bc
Jakub Cichon <jakub@uconn.edu>
2022-12-23 23:26:44 -0500

    add helper functions for file name parsing and getting department id

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,23 @@
-module.exports = {
-    format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(
-        date
-      ).getDate()}/${new Date(date).getFullYear()}`;
-    },
-  };
-  
+// get all departments
+const getDepartments = async () => {
+	const response = await fetch('/api/departments');
+	const departments = await response.json();
+	return departments;
+};
+
+getDepartmentId = async (departmentName) => {
+  const departments = await getDepartments();
+  const department = departments.find(
+    (department) => department.department_name === departmentName
+  );
+  return department.id;
+};
+
+const parseFileName = (name) => {
+	return name
+		.replace(/[^a-zA-Z0-9 ]/g, '')
+		.replace(/\s/g, '_')
+		.toLowerCase();
+};
+
+module.exports = { getDepartments, getDepartmentId, parseFileName };
