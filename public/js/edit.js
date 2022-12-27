ddd3226eb07d911d781666bd9b51f2cccd63bced
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-12-26 19:15:25 -0500

    added functionallity to the edit button

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,32 +1,34 @@
 const editFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // these will have to change
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
-    
-  
-    const id = window.location.toString().split("/")[
-      window.location.toString().split("/").length - 1
-    ];
-  
-    const response = await fetch(`/api/products/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        post_id: id,
-        title,
-        content,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector("").addEventListener("submit", editFormHandler);
+  event.preventDefault();
+
+  const productName = document.getElementById("pName").value.trim();
+  const productPrice = document.getElementById("pPrice").value;
+  const stockAmount = document.getElementById("pStock").value;
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`/api/products/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      product_name: productName,
+      price: productPrice,
+      stock: stockAmount,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+    alert("Product updated!");
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document
+  .querySelector("#editConfirm")
+  .addEventListener("click", editFormHandler);
