8fb0d1f9108853770ef431b9c71a34fba05c99c0
rjo6615 <robertjobernier@gmail.com>
2022-12-28 12:44:11 -0500

    got signup working

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,11 +2,15 @@ const signupFormHandler = async (event) => {
   event.preventDefault();
 
   // need to collect the selectors for the email and password
-  const email = document.getElementById("").value.trim();
-  const password = document.getElementById("").value.trim();
+  const email = document.getElementById("signup-email").value.trim();
+  const password = document.getElementById("signup-password").value.trim();
+  const passwordConfirm = document.getElementById("signup-confirm").value.trim();
 
   if (email && password) {
-    const response = await fetch("/api/users", {
+    if(password !== passwordConfirm) {
+      alert("Passwords do not match");
+    } else {
+    const response = await fetch("/api/users/signup", {
       method: "POST",
       body: JSON.stringify({
         email,
@@ -19,10 +23,11 @@ const signupFormHandler = async (event) => {
 
       document.location.replace("/");
     } else {
-      alert(response.statusText);
+      alert("failed to signup");
     }
   }
+}
 };
 
 // need to collect the selector for the signup form
-document.querySelector("").addEventListener("submit", signupFormHandler);
+document.querySelector("#signupConfirm").addEventListener("click", signupFormHandler);
