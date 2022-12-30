5124b177d4ad1ca3b15c307ab2452f8aa5419f92
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-12-30 07:26:43 -0500

    deleted unused route and added session funcationality to the existing one

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -43,32 +43,16 @@ router.post('/signup', async (req, res) => {
 			password: req.body.password,
 		});
 
+    req.session.save(() => {
+      req.session.user_id = signupData.id;
+      req.session.email = signupData.email;
+      req.session.loggedIn = true;
+    });
+
 		res.json(signupData);
 	} catch (err) {
 		res.status(500).json(err);
 	}
-});
-
-// create a user route for api/users/
-router.post("/", async (req, res) => {
-  try {
-    const userData = await User.create({
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.username = userData.username;
-      req.session.loggedIn = true;
-
-      res.json(userData);
-
-      console.log(userData);
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 // reset password route
