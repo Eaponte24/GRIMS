b6314efe5e01d7a928f1b5af3076db39913841c5
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-01-04 10:54:10 -0500

    added a welcome scren only to the login page

---
+++ public/js/welcomeScreen.js
@@ -0,0 +1,8 @@
+const splash = document.querySelector(".splash");
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  setTimeout(() => {
+    splash.classList.add("display-none");
+  }, 2000);
+});
+
